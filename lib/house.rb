cccd5ea0ee3e40ccdfaefa1fbac563665f8d9944
Aidan Murray <aidanhansm@gmail.com>
2020-08-26 09:33:12 -0600

    Add House class and attributes

---
+++ lib/house.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Describes a house with rooms, a price, and an address
+class House
+  attr_reader :price, :address, :rooms
+
+  def initialize(price, address)
+    @price = price.tr('^0-9', '').to_i
+    @address = address
+    @rooms = []
+  end
+end
